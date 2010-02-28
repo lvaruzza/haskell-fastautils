d82129f9929e58e3c97446048df5f2303443d553
Leonardo Varuzza <varuzza@gmail.com>
2010-02-28 19:23:33 -0300

    Transform countUniqSeqs in a filter

--- Common.hs
+++ Common.hs
@@ -1,5 +1,6 @@
 module Common (
-               doFilter
+               doFilter,
+               applySnd2               
 ) where 
 
 import System.IO
@@ -18,3 +19,8 @@ doFilter conversor = do
               input <- (openFile inputFile ReadMode) 
               conversor input stdout
     otherwise -> conversor stdin stdout
+
+--- Apply f x y to snd of x and y
+applySnd2 :: (b -> b -> c) -> (a,b) -> (a,b) -> c
+applySnd2 f x y = f (snd x) (snd y)
+
--- countUniqSeqs.hs
+++ countUniqSeqs.hs
@@ -9,6 +9,7 @@ import qualified Data.ByteString.Lazy as B
 import qualified Data.ByteString.Lazy.Char8 as BB
 import System.IO
 import System (getArgs)
+import Common
 
 type SeqMap = M.Map SeqData Integer
 
@@ -27,23 +28,15 @@ countSeqs handle = do
   seqs <- liftIO $ hReadFasta handle
   mapM_ countSeq seqs
 
-countSeqsFile :: Handle -> IO ()
-countSeqsFile handle = do
-     counts <- execStateT (countSeqs handle) M.empty
+doCountSeq :: Handle -> Handle -> IO ()
+doCountSeq input output = do
+     counts <- execStateT (countSeqs input) M.empty
 
-     let lst = sortBy sortFun (M.toList counts)
-                      where sortFun (_,a1) (_,a2) = compare a2 a1 
+     let lst = sortBy (applySnd2 compare) (M.toList counts)
 
-     putStr $ foldl printEntry "" lst
-                    where
-                        printEntry buff (k, a) = buff ++ (BB.unpack k) ++ "\t" ++ 
-                                                       (show a) ++ "\n"
+     hPutStr output $ foldl printEntry "" lst
+         where
+           printEntry buff (k, a) = buff ++ (BB.unpack k) ++ "\t" ++ 
+                                    (show a) ++ "\n"
                         
-main = do
-     args <- getArgs
-     case args of
-          [path] -> do
-                 handle <- openFile path ReadMode
-                 countSeqsFile handle
-                 hClose handle
-          otherwise -> countSeqsFile stdin
+main = doFilter doCountSeq
