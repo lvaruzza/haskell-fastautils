a3aaa89f9d93344a2ef0406b0c56336af3dfbe49
Leonardo Varuzza <varuzza@gmail.com>
2010-02-28 14:00:14 -0300

    Change build system to Cabal

--- make.sh
+++ make.sh
@@ -1,4 +1,6 @@
-ghc --make countFasta.hs
-ghc --make fastaStat.hs
-ghc --make fastq2qual.hs
-ghc --make fastq2fasta.hs
+#ghc --make countFasta.hs
+#ghc --make fastaStat.hs
+#ghc --make fastq2qual.hs
+#ghc --make fastq2fasta.hs
+
+./Setup.hs configure --user && ./Setup.hs build
